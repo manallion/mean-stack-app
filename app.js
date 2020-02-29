b77052ff06e6ef8cf6813f16abc63014fc9f093e
manal <manallion@yahoo.com>
2020-02-29 21:13:24 +0200

    Deployment changes done

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var node=require('node');
 var app=node();
 
 app.get('/',function(request,response){
-  response.sendFile(__dirname+'/index.html');
+  response.sendFile(__dirname+'/index2.html');
 });
 
 var port = process.env.PORT || 8080;
